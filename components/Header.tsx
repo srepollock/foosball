6308c30a1e95cf00edcb8ada2a141ff9b3eca65c
Spencer Pollock <spencer@spollock.ca>
2024-05-17 13:09:08 -0700

    fix: main screen

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,30 +6,8 @@ export default function Header() {
         <div className="flex flex-col gap-16 items-center">
             <h1 className="sr-only">?</h1>
             <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-                A new way to fooseball
+                A new way to foosball
             </p>
-            <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
-            <div className="flex gap-4">
-                <p>Record:</p>
-            </div>
-            <div className="flex gap-4">
-                <p>
-                    <ul>
-                        <li>
-                            <p>Matches</p>
-                        </li>
-                        <li>
-                            <p>Players</p>
-                        </li>
-                        <li>
-                            <p>Teams</p>
-                        </li>
-                        <li>
-                            <p>And More!</p>
-                        </li>
-                    </ul>
-                </p>
-            </div>
         </div>
     );
 }
