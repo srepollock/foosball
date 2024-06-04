939b80a9d66de5da900d22330f4df383a56372e4
Spencer Pollock <spencer@spollock.ca>
2024-06-04 09:33:36 -0700

    fix: added leaderboard page

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,17 +1,13 @@
 import Footer from "@/components/Footer";
 import PageHeader from "@/components/PageHeader";
-import TeamDetails from "@/components/teams/TeamDetails";
+import Leaderboard from "@/components/leaderboard/Leaderboard";
 
-export default function TeamDetailsPage({
-    params,
-}: {
-    params: { id: string };
-}) {
+export default function LeaderboardPage() {
     return (
         <div className="flex-1 w-full flex flex-col gap-20 items-center">
-            <PageHeader pageName="Teams" />
+            <PageHeader pageName="Leaderboards" />
             <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-                <TeamDetails id={params.id} />
+                <Leaderboard />
             </div>
             <Footer />
         </div>
--- app/team/page.tsx
+++ app/team/page.tsx
@@ -6,7 +6,7 @@ import TeamList from "@/components/teams/TeamList";
 export default function TeamPage() {
     return (
         <div className="flex-1 w-full flex flex-col gap-20 items-center">
-            <PageHeader pageName="Tournaments" />
+            <PageHeader pageName="Teams" />
             <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
                 <TeamList />
             </div>
