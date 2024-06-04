6a064335d97818b50ee101cd7ddd85dcf81bcbba
Spencer Pollock <spencer@spollock.ca>
2024-06-04 09:34:43 -0700

    fix: tournament form

--- app/tournament/page.tsx
+++ app/tournament/page.tsx
@@ -2,6 +2,7 @@ import AuthButton from "@/components/AuthButton";
 import Footer from "@/components/Footer";
 import PageHeader from "@/components/PageHeader";
 import NavMenu from "@/components/navmenu/NavMenu";
+import NewTournamentForm from "@/components/tournaments/NewTournamentForm";
 import TournamentDetails from "@/components/tournaments/TournamentDetails";
 import TournamentList from "@/components/tournaments/TournamentList";
 
@@ -11,6 +12,7 @@ export default function TournamentDashboard() {
             <PageHeader pageName="Tournaments" />
             <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
                 <TournamentList />
+                <NewTournamentForm />
             </div>
             <Footer />
         </div>
